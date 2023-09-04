7c7a9af565376e7c1acbd1e66c89e1d207643cc9
REDKINVA <59052910+RVR2D2@users.noreply.github.com>
2023-09-04 12:53:18 +0600

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { faker } from '@faker-js/faker';
+import './App.css'
+import {ReactComponent as AvatarIcon} from "./Avatar.svg";
+import { CircularProgress } from '@mui/material';
 
-function App() {
+const TodoColumn: React.FC = () => {
+  const [todos, setTodos] = useState([]);
+  const [loadedTasksCount, setLoadedTasksCount] = useState(0);
+
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/todos')
+    .then((response) => {
+      setTodos(response.data);
+      setLoadedTasksCount(response.data.length);
+    })
+    .catch((error) => {
+      console.error('Error fetching data:', error);
+    });
+  }, []);
+
+  const generateRandomDescription = () => faker.lorem.sentence();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todo-column">
+      <div className="column-header">
+        <span>Today ({loadedTasksCount})</span>
+        <button disabled>+</button>
+      </div>
+      {todos.length
+        ? <div className="task-list">
+        {todos.map((todo: any) => (
+          <div className="task" key={todo.id}>
+            <div className="task-title">
+              <input type="checkbox" checked={todo.completed} readOnly />
+              {todo.title}
+            </div>
+            <div className="task-description">{generateRandomDescription()}</div>
+
+            <div className="task-dates">
+              <div className="date">startDate: {faker.date.past().toLocaleDateString()}</div>
+              <div className="date">endDate: {faker.date.future().toLocaleDateString()}</div>
+            </div>
+            <div className='task-block'>
+              <div className="task-tags">
+                <div className="tag purple">Entity title</div>
+                <div className="tag gray">Front-end</div>
+              </div>
+              <div className="user-avatar">
+                <AvatarIcon/>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div> : <CircularProgress color="success" />}
+
     </div>
   );
-}
+};
 
-export default App;
+export default TodoColumn;
